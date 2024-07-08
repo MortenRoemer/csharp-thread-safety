ae2109ad1f84937df0fc5c775b01ba45f965cf09
Morten Römer <morten.roemer@gmx.de>
2024-07-08 17:56:23 +0200

    fixed bug mentioned in issue#2

---
+++ MortenRoemer.ThreadSafetyTagging.Examples/ExampleEnum.cs
@@ -0,0 +1,17 @@
+namespace MortenRoemer.ThreadSafetyTagging.Examples;
+
+public enum ExampleEnum
+{
+    Unknown = 0, 
+    Sender = 1,
+    ToRecipient = 2,
+    CcRecipient = 3,
+    BccRecipient = 4,
+    RequiredAttendee = 5,
+    OptionalAttendee = 6,
+    Organizer = 7,
+    Regarding = 8,
+    Owner = 9,
+    Resource = 10,
+    Customer = 11
+}
--- MortenRoemer.ThreadSafetyTagging.Examples/SynchronizedDataClass.cs
+++ MortenRoemer.ThreadSafetyTagging.Examples/SynchronizedDataClass.cs
@@ -5,6 +5,12 @@ namespace MortenRoemer.ThreadSafetyTagging.Examples;
 [SynchronizedMemoryAccess]
 public sealed class SynchronizedDataClass
 {
+    // Any Enum property that is read-only is immutable and therefore safe
+    public ExampleEnum EnumValueProperty { get; }
+    
+    // Any Enum property that is read-only is immutable and therefore safe
+    public readonly ExampleEnum EnumValueField;
+    
     // Any class that is already thead-safe can be used in a synchronized context
     public readonly HttpClient _client = new();
     
